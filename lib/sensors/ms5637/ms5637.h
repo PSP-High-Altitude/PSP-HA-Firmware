0ec932cd59bcaf5df3df16c3329c18c6175de6dc
rmalavat <rmalavat@purdue.edu>
2022-10-05 12:26:11 -0400

    Created sensor data headers

--- lib/sensors/ms5637/ms5637.h
+++ lib/sensors/ms5637/ms5637.h
@@ -1,4 +1,17 @@
 #ifndef MS5637_H
 #define MS5637_H
 
+#include <stdint.h>
+
+typedef struct {
+    uint16_t sens;     // Pressure Sensitivity
+    uint16_t off;      // pressure offset
+    uint16_t tcs;      // Temperature coefficient of pressure sensitivity
+    uint16_t tco;      // Temperature coefficient of pressure offset
+    uint16_t tref;     // Reference Temperature
+    uint16_t tempsens; // Temperature coefficient of the temperature
+} calibration; // This struct is used to store the calibrations values used in
+               // order to calculate the pressure and temperature in usable
+               // units (mbar and C)
+
 #endif // MS5637_H
