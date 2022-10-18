fae34e1c4869a796dfe52d297d37fe2bc4522037
griffinross2 <griffinross2@gmail.com>
2022-10-18 19:49:22 -0400

    Create ms5637 driver implementation

--- lib/sensors/data.h
+++ lib/sensors/data.h
@@ -6,27 +6,24 @@
 // Barometer MS5637
 
 typedef struct {
-    int16_t
-        temperature; // After we do the calculations, we should get a signed int
-                     // 16 for temp. The range would be between -4000 to 8500 C
-} Temperature;
-
-typedef struct {
-    uint32_t pressure; // After calculations. the pressure should be a signed
-                       // int 32 with a range of 1000 to 120000 mbar
-} Pressure;
+    float
+        temperature;  // After we do the calculations, we should get a float for
+                      // temp. The range would be between -40.00 to 85.00 C
+    float pressure;   // After calculations. the pressure should be a float with
+                      // a range of 10.00 to 1200.00 mbar
+} BaroData;
 
 // IMU LSM6
 
 typedef struct {
-    uint16_t accelX; // 16 bit linear acceleration sensor x-axis in g
-    uint16_t accelY; // 16 bit linear acceleration sensor y-axis in g
-    uint16_t accelZ; // 16 bit linear acceleration sensor z-axis in g
+    float accelX;  // linear acceleration sensor x-axis in g
+    float accelY;  // linear acceleration sensor y-axis in g
+    float accelZ;  // linear acceleration sensor z-axis in g
 } Accel;
 
 typedef struct {
-    uint16_t gyroX; // 16 bit anglular rate sensor yaw x-axis in dps
-    uint16_t gyroY; // 16 bit anglular rate sensor yaw y-axis in dps
-    uint16_t gyroZ; // 16 bit anglular rate sensor yaw z-axis in dps
+    float gyroX;  // anglular rate sensor yaw x-axis in dps
+    float gyroY;  // anglular rate sensor yaw y-axis in dps
+    float gyroZ;  // anglular rate sensor yaw z-axis in dps
 } Gyro;
 #endif // DATA_H
---
+++ lib/sensors/ms5637/ms5637.c
@@ -0,0 +1,159 @@
+#include "ms5637.h"
+
+I2cDevice *device;
+CalibrationData data;
+
+Status ms5637_init(I2cDevice *dev) {
+    device = dev;
+
+    uint8_t rx_buf[2];
+    uint8_t tx_buf[1] = {0x1E};
+
+    // Send Reset Command
+    if (i2c_write(device, tx_buf, 1) != OK) {
+        return ERROR;
+    };
+
+    // Read Constants
+    tx_buf[0] = 0xA0;
+
+    // Read C1
+    tx_buf[0] |= (1 << 1);
+    if (i2c_write(device, tx_buf, 1) != OK) {
+        return ERROR;
+    };
+    if (i2c_read(device, rx_buf, 2) != OK) {
+        return ERROR;
+    };
+    data.C1 = (uint16_t)rx_buf[0] | rx_buf[1];
+
+    // Read C2
+    tx_buf[0] |= (2 << 1);
+    if (i2c_write(device, tx_buf, 1) != OK) {
+        return ERROR;
+    };
+    if (i2c_read(device, rx_buf, 2) != OK) {
+        return ERROR;
+    };
+    data.C2 = (uint16_t)rx_buf[0] | rx_buf[1];
+
+    // Read C3
+    tx_buf[0] |= (3 << 1);
+    if (i2c_write(device, tx_buf, 1) != OK) {
+        return ERROR;
+    };
+    if (i2c_read(device, rx_buf, 2) != OK) {
+        return ERROR;
+    };
+    data.C3 = (uint16_t)rx_buf[0] | rx_buf[1];
+
+    // Read C4
+    tx_buf[0] |= (4 << 1);
+    if (i2c_write(device, tx_buf, 1) != OK) {
+        return ERROR;
+    };
+    if (i2c_read(device, rx_buf, 2) != OK) {
+        return ERROR;
+    };
+    data.C4 = (uint16_t)rx_buf[0] | rx_buf[1];
+
+    // Read C5
+    tx_buf[0] |= (5 << 1);
+    if (i2c_write(device, tx_buf, 1) != OK) {
+        return ERROR;
+    };
+    if (i2c_read(device, rx_buf, 2) != OK) {
+        return ERROR;
+    };
+    data.C5 = (uint16_t)rx_buf[0] | rx_buf[1];
+
+    // Read C6
+    tx_buf[0] |= (6 << 1);
+    if (i2c_write(device, tx_buf, 1) != OK) {
+        return ERROR;
+    };
+    if (i2c_read(device, rx_buf, 2) != OK) {
+        return ERROR;
+    };
+    data.C6 = (uint16_t)rx_buf[0] | rx_buf[1];
+
+    return OK;
+}
+
+static uint32_t read_D1(AdcSpeed speed) {
+    uint8_t rx_buf[2] = {0, 0};
+    uint8_t tx_buf[1] = {0x40 | speed};
+    if (i2c_write(device, tx_buf, 1) != OK) {
+        return D_READ_ERROR;
+    }
+    while ((uint16_t)rx_buf[0] | rx_buf[1]) {
+        if (i2c_read(device, rx_buf, 2) != OK) {
+            return D_READ_ERROR;
+        }
+    }
+    uint32_t D1 = (uint16_t)rx_buf[0] | rx_buf[1];
+    return D1;
+}
+
+static uint32_t read_D2(AdcSpeed speed) {
+    uint8_t rx_buf[2] = {0, 0};
+    uint8_t tx_buf[1] = {0x50 | speed};
+    if (i2c_write(device, tx_buf, 1) != OK) {
+        return D_READ_ERROR;
+    }
+    while ((uint16_t)rx_buf[0] | rx_buf[1]) {
+        if (i2c_read(device, rx_buf, 2) != OK) {
+            return D_READ_ERROR;
+        }
+    }
+    uint32_t D2 = (uint16_t)rx_buf[0] | rx_buf[1];
+    return D2;
+}
+
+BaroData ms5637_read(AdcSpeed speed) {
+    BaroData result;
+    result.pressure = NAN;
+    result.temperature = NAN;
+
+    uint32_t D1;
+    uint32_t D2;
+
+    if (D1 = readD1(speed) == D_READ_ERROR) {
+        return result;
+    }
+    if (D2 = readD2(speed) == D_READ_ERROR) {
+        return result;
+    }
+    int32_t dT = D2 - (data.C5 * 256);
+    int32_t TEMP = (int32_t)(2000 + (dT * ((float)data.C6 / 8388608)));
+    int64_t OFF = (data.C2 * 131072) + ((data.C4 * dT) / 64);
+    int64_t SENS = (data.C1 * 65536) + ((data.C3 * dT) / 128);
+    int32_t T2;
+    int64_t OFF2;
+    int64_t SENS2;
+    if ((float)TEMP / 100 < 20.00) {
+        T2 = 3 * (dT * dT) / 8589934592;
+        OFF2 = 61 * (TEMP - 2000) * (TEMP - 2000) / 16;
+        SENS2 = 29 * (TEMP - 2000) * (TEMP - 2000) / 16;
+        if ((float)TEMP / 100 < -15.00) {
+            OFF2 += 17 * (TEMP + 1500) * (TEMP + 1500);
+            SENS2 += 9 * (TEMP + 1500) * (TEMP + 1500);
+        }
+    } else {
+        T2 = 5 * (dT * dT) / 274877906944;
+        OFF2 = 0;
+        SENS2 = 0;
+    }
+    TEMP -= T2;
+    OFF -= OFF2;
+    SENS -= SENS2;
+
+    int32_t P = ((D1 * SENS / 2097152) - OFF) / 32768;
+
+    BaroData result;
+
+    result.pressure = (float)P / 100;
+    result.temperature = (float)TEMP / 100;
+
+    return result;
+}
