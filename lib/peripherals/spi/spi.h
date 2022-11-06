1a5ee618132cff9c89a2eac356a7b59fcc377762
OxyMagnesium <im.omgupta@gmail.com>
2022-11-06 12:01:33 -0500

    Move SPI structs into single header file

--- lib/peripherals/spi/spi.h
+++ lib/peripherals/spi/spi.h
@@ -8,8 +8,20 @@
 // PSPHAA library
 #include <status.h>
 
-// Local headers
-#include "device.h"
+typedef enum {
+    SPI0 = 0,
+    SPI1 = 1,
+    SPI2 = 2,
+    SPI3 = 3,
+} SpiPeriph;
+
+typedef struct {
+    uint32_t clk;  // Clock target (in Hz)
+    bool cpol;
+    bool cpha;
+    uint8_t cs;
+    SpiPeriph periph;
+} SpiDevice;
 
 Status spi_exchange(SpiDevice* dev, uint8_t* tx_buf, uint8_t* rx_buf,
                     uint8_t len);
