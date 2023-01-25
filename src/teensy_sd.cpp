7d32013212fd4397104823652d8f127207a105e3
OxyMagnesium <im.omgupta@gmail.com>
2023-01-24 21:52:08 -0500

    Add stopgap SD write function

--- src/teensy_sd.cpp
+++ src/teensy_sd.cpp
@@ -1,30 +1,54 @@
 #include "teensy_sd.h"
 
-static File s_file;
+static char s_filename[12] = "data_00.csv";
 
 Status sd_init(SDDevice* device) {
     if (!SD.begin(device->cs)) {
         return HARDWARE_ERROR;
     }
 
-    char filename[8] = "data_00";
-
     // Increment the suffix of the filename until we find an unused name
     // This is a super shitty and inefficient way to do this, but this is also
     // supposed to be stopgap code that runs on a 600 MHz Cortex-M7, so eh
-    while (SD.exists(filename)) {
-        if (filename[6] == '9') {
-            filename[5] += 1;
-            filename[6] = '0';
+    while (SD.exists(s_filename)) {
+        if (s_filename[6] == '9') {
+            s_filename[5] += 1;
+            s_filename[6] = '0';
         } else {
-            filename[6] += 1;
+            s_filename[6] += 1;
         }
     }
 
-    s_file = SD.open(filename, FILE_WRITE);
-    if (!s_file) {
+    File file = SD.open(s_filename, FILE_WRITE);
+    if (!file) {
         return ERROR;
     }
 
+    file.println(
+        "Timestamp,"
+        "Ax,Ay,Az,"
+        "Rx,Ry,Rz,"
+        "Temp,"
+        "Pressure");
+
+    file.close();
+
     return OK;
+}
+
+Status sd_write(uint64_t timestamp, Accel* accel, Gyro* gyro, BaroData* baro) {
+    File file = SD.open(s_filename, FILE_WRITE);
+
+    int retval =
+        file.printf("%d,%f,%f,%f,%f,%f,%f,%f,%f\n", timestamp, accel->accelX,
+                    accel->accelY, accel->accelZ, gyro->gyroX, gyro->gyroY,
+                    gyro->gyroZ, baro->temperature, baro->pressure);
+
+    file.close();
+
+    if (retval > 0) {
+        return OK;
+    } else {
+        return ERROR;
+    }
 }
--- src/teensy_sd.h
+++ src/teensy_sd.h
@@ -4,6 +4,7 @@
 #include <SD.h>
 #include <stdint.h>
 
+#include "data.h"
 #include "status.h"
 
 typedef struct {
@@ -11,5 +12,7 @@ typedef struct {
 } SDDevice;
 
 Status sd_init(SDDevice* device);
+
+Status sd_write(uint64_t timestamp, Accel* accel, Gyro* gyro, BaroData* baro);
 
 #endif  // TEENSY_SD_H
