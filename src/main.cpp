c6a05b028be55d5025a1e1044ccf8ec665b8a415
griffinross2 <griffinross2@gmail.com>
2023-02-03 17:05:03 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,15 @@ extern "C" {
 
 #define LED_PIN 13
 
+uint32_t lastTime = 0;
+
 I2cDevice baroConf = {
     .address = 0x76,
     .clk = I2C_SPEED_STANDARD,
-    .periph = I2C0,
+    .periph = I2C3,
 };
 SpiDevice imuConf = {
-    .clk = 100000,
+    .clk = 1000000,
     .cpol = 0,
     .cpha = 0,
     .cs = 0,
@@ -26,47 +28,30 @@ SpiDevice imuConf = {
 
 void setup() {
     initTimers();
-    Serial.begin(9600);
     gpio_mode(LED_PIN, GPIO_OUTPUT);
+    gpio_mode(2, GPIO_INPUT);
     delay(200);
     ms5637_init(&baroConf);
     lsm6dsox_init(&imuConf);
-    lsm6dsox_config_accel(&imuConf, LSM6DSOX_XL_RATE_52_HZ,
-                          LSM6DSOX_XL_RANGE_2_G);
-    lsm6dsox_config_gyro(&imuConf, LSM6DSOX_G_RATE_52_HZ,
-                         LSM6DSOX_G_RANGE_250_DPS);
+    lsm6dsox_config_accel(&imuConf, LSM6DSOX_XL_RATE_208_HZ,
+                          LSM6DSOX_XL_RANGE_8_G);
+    lsm6dsox_config_gyro(&imuConf, LSM6DSOX_G_RATE_208_HZ,
+                         LSM6DSOX_G_RANGE_500_DPS);
     SDDevice device{.cs = BUILTIN_SDCARD};
     sd_init(&device);
 }
 
 void loop() {
-    /*
-    uint16_t val = 0x4841;
-    for (size_t i = 0; i < 16; i++) {
-        digitalWrite(LED_PIN, HIGH);
-        delay(250);
-        digitalWrite(LED_PIN, LOW);
-        delay(250);
-        digitalWrite(LED_PIN, (val >> i) & 1);
-        delay(500);
-        digitalWrite(LED_PIN, LOW);
-        delay(1000);
+    while (gpio_read(2) == GPIO_HIGH) {
+        gpio_write(LED_PIN, GPIO_HIGH);
     }
-    */
-    Serial.println(MILLIS());
-    gpio_write(LED_PIN, GPIO_HIGH);
-    DELAY(500);
+    while (MILLIS() - lastTime < 10) {
+    }
+    lastTime = MILLIS();
     gpio_write(LED_PIN, GPIO_LOW);
-    DELAY(500);
     Accel accelData = lsm6dsox_read_accel(&imuConf);
-    Serial.printf("Accel X: %f g, Accel Y: % f g, Accel Z: %f g\n",
-                  accelData.accelX, accelData.accelY, accelData.accelZ);
     Gyro gyroData = lsm6dsox_read_gyro(&imuConf);
-    Serial.printf("Gyro X: %f dps, Gyro Y: % f dps, Gyro Z: %f dps\n",
-                  gyroData.gyroX, gyroData.gyroY, gyroData.gyroZ);
     BaroData baroData = ms5637_read(&baroConf, OSR_256);
-    Serial.printf("Pressure: %f mbar, Temperature %f degC\n", baroData.pressure,
-                  baroData.temperature);
 
     // Accel accelData = {.accelX = 0.123, .accelY = 0.211, .accelZ = 9.814};
     // Gyro gyroData = {.gyroX = 0.025, .gyroY = 0.011, .gyroZ = 0.0352};
