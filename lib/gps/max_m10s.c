b78fbd09511b0a6fbfbffa459130caa78dfd8522
griffinross2 <griffinross2@gmail.com>
2023-02-24 20:14:21 -0500

    Added VALSET command to max-m10s library

--- lib/gps/max_m10s.c
+++ lib/gps/max_m10s.c
@@ -5,7 +5,21 @@
 static uint64_t* ubx_cfg_valget(I2cDevice*, Max_M10S_Layer_TypeDef, uint32_t*,
                                 size_t);
 
-Status max_m10s_init(I2cDevice* device) {}
+static Status ubx_cfg_valset(I2cDevice* device, Max_M10S_Layer_TypeDef layer,
+                             uint32_t* keys, uint64_t* values,
+                             uint8_t* value_lens, size_t num_items);
+
+Status max_m10s_init(I2cDevice* device) {
+    uint32_t keys[] = {0x20110021};
+    uint64_t values[] = {8};
+    uint8_t value_lens[] = {1};
+    if (ubx_cfg_valset(device, MAX_M10S_LAYER_SET_RAM, keys, values, value_lens,
+                       1) == STATUS_OK) {
+        printf("Set nav mode to airborne <4G");
+    } else {
+        return STATUS_ERROR;
+    }
+}
 
 static uint64_t* ubx_cfg_valget(I2cDevice* device, Max_M10S_Layer_TypeDef layer,
                                 uint32_t* keys, size_t num_keys) {
@@ -15,14 +29,14 @@ static uint64_t* ubx_cfg_valget(I2cDevice* device, Max_M10S_Layer_TypeDef layer,
     tx_buf[1] = 0x62;
     tx_buf[2] = 0x06;
     tx_buf[3] = 0x8B;
-    tx_buf[4] = ((4 * num_keys) >> 8) & 0xFF;
-    tx_buf[5] = (4 * num_keys) & 0xFF;
+    tx_buf[4] = ((4 + 4 * num_keys) >> 8) & 0xFF;
+    tx_buf[5] = (4 + 4 * num_keys) & 0xFF;
     tx_buf[6] = 0;
     tx_buf[7] = (uint8_t)layer;
     tx_buf[8] = 0;
     tx_buf[9] = 0;
     for (uint16_t i = 0; i < num_keys; i++) {
-        for (uint8_t j = 0; j < 3; j++) {
+        for (uint8_t j = 0; j < 4; j++) {
             tx_buf[10 + (i * 4) + j] = (keys[i] >> (j * 8)) & 0xFF;
         }
     }
@@ -72,4 +86,72 @@ static uint64_t* ubx_cfg_valget(I2cDevice* device, Max_M10S_Layer_TypeDef layer,
     free(message_buf);
 
     return values;
+}
+
+static Status ubx_cfg_valset(I2cDevice* device, Max_M10S_Layer_TypeDef layer,
+                             uint32_t* keys, uint64_t* values,
+                             uint8_t* value_lens, size_t num_items) {
+    uint16_t total_len;
+    for (uint8_t i = 0; i < num_items; i++) {
+        total_len += 4 + value_lens[i];
+    }
+    size_t tx_buf_len = 12 + total_len;
+    uint8_t* tx_buf = malloc(tx_buf_len);
+    tx_buf[0] = 0xB5;
+    tx_buf[1] = 0x62;
+    tx_buf[2] = 0x06;
+    tx_buf[3] = 0x8A;
+    tx_buf[4] = ((4 + total_len) >> 8) & 0xFF;
+    tx_buf[5] = (4 + total_len) & 0xFF;
+    tx_buf[6] = 0;
+    tx_buf[7] = (uint8_t)layer;
+    tx_buf[8] = 0;
+    tx_buf[9] = 0;
+    uint16_t tx_buf_idx = 10;
+    for (uint16_t i = 0; i < total_len; i++) {
+        for (uint8_t j = 0; j < 4; j++) {
+            tx_buf[tx_buf_idx] = (keys[i] >> (j * 8)) & 0xFF;
+            tx_buf_idx += 1;
+        }
+        for (uint8_t j = 0; j < value_lens[i]; j++) {
+            tx_buf[tx_buf_idx] = (values[i] >> (j * 8)) & 0xFF;
+            tx_buf_idx += 1;
+        }
+    }
+    uint8_t CK_A = 0;
+    uint8_t CK_B = 0;
+    for (uint16_t i = 0; i < tx_buf_len - 2; i++) {
+        CK_A += tx_buf[i];
+        CK_B += CK_A;
+    }
+    tx_buf[tx_buf_len - 2] = CK_A;
+    tx_buf[tx_buf_len - 1] = CK_B;
+
+    i2c_write(device, tx_buf, tx_buf_len);
+    free(tx_buf);
+
+    uint32_t message_header = 0;
+    uint8_t* message_buf;
+    uint16_t bytes_avail = 0;
+    while (message_header != 0x010562B5 && message_header != 0x000562B5) {
+        bytes_avail = 0;
+        while (!bytes_avail) {
+            uint8_t addr_buf[1] = {0xFD};
+            uint8_t rx_buf[2];
+            i2c_write(device, addr_buf, 1);
+            i2c_read(device, rx_buf, 2);
+            bytes_avail = (rx_buf[0] << 8) + rx_buf[1];
+        }
+        message_buf = malloc(bytes_avail);
+        i2c_read(device, message_buf, bytes_avail);
+        for (uint8_t i = 0; i < 3; i++) {
+            message_header = message_buf[i] << (i * 8);
+        }
+    }
+
+    if (message_header == 0x010562B5) {
+        return STATUS_OK;
+    } else {
+        return STATUS_ERROR;
+    }
 }
--- lib/gps/max_m10s.h
+++ lib/gps/max_m10s.h
@@ -4,11 +4,17 @@
 #include "i2c/i2c.h"
 #include "status.h"
 
+#define
+
 typedef enum {
-    MAX_M10S_LAYER_RAM = 0,
-    MAX_M10S_LAYER_BBR = 1,
-    MAX_M10S_LAYER_FLASH = 2,
-    MAX_M10S_LAYER_DEFAULT = 7,
+    MAX_M10S_LAYER_GET_RAM = 0,
+    MAX_M10S_LAYER_GET_BBR = 1,
+    MAX_M10S_LAYER_GET_FLASH = 2,
+    MAX_M10S_LAYER_GET_DEFAULT = 7,
+    MAX_M10S_LAYER_SET_RAM = 1,
+    MAX_M10S_LAYER_SET_BBR = 2,
+    MAX_M10S_LAYER_SET_FLASH = 4,
+    MAX_M10S_LAYER_SET_ALL = 7,
 } Max_M10S_Layer_TypeDef;
 
 Status max_m10s_init(I2cDevice* device);
