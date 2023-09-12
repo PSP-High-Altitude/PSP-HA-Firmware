3f5f5af4ce69681f04d2dbb4b6fda30c51045ca2
OxyMagnesium <im.omgupta@gmail.com>
2023-09-12 19:43:59 -0400

    Add FreeRTOS path in timer delay

--- lib/sensors/lsm6dsox/lsm6dsox.c
+++ lib/sensors/lsm6dsox/lsm6dsox.c
@@ -26,8 +26,6 @@ static Status lsm6dsox_read(SpiDevice* device, uint8_t address, uint8_t* rx_buf,
     Status status = spi_exchange(device, tx_buf, rx_buf_new, len + 1);
     memcpy(rx_buf, rx_buf_new + 1, len);
 
-    DELAY_MICROS(100);
-
     return status;
 }
 
@@ -49,8 +47,6 @@ static Status lsm6dsox_write(SpiDevice* device, uint8_t address,
     // Exchange address and write len bytes
     Status status = spi_exchange(device, tx_buf_new, rx_buf, len + 1);
 
-    DELAY_MICROS(100);
-    
     return status;
 }
 
