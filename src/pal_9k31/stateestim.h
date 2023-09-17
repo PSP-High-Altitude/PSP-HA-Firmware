58c44f2df59f7c5b2625f730cf155fd2754c0bb9
wilczewskiadam <adamwilczewski137@gmail.com>
2023-09-17 13:29:37 -0400

    Fixed header

--- src/pal_9k31/stateestim.h
+++ src/pal_9k31/stateestim.h
@@ -12,15 +12,7 @@ typedef struct {
     Vector v;
 } Quaternion;
 
-Quaternion QuatStep(Quaternion q, Vector w, float dt);
-float vdot(Vector v1, Vector v2);
-Vector vcross(Vector v1, Vector v2);
-Vector vscale(Vector v, float k);
-Vector vadd(Vector v1, Vector v2);
-Vector QuatRot(Vector v, Quaternion qn);
-Vector TrapInt(Vector vint, Vector v, Vector vprev, float dt);
-
-Quaternion DegEul2Quat(float roll, float pitch, float yaw);
-void UpdatePose(Vector v_a, Vector v_w, float dt, Vector *v_ac, Vector *v_v,
-                Vector *v_d, Quaternion *q_rot);
+Quaternion deg_eul_to_quat(float roll, float pitch, float yaw);
+void update_pose(Vector v_a, Vector v_w, float dt, Vector *v_ac, Vector *v_v,
+                 Vector *v_d, Quaternion *q_rot);
 #endif
