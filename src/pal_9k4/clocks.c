cbbd6be225975c115a2d5f6aa0373e4e8b051491
griffinross2 <griffinross2@gmail.com>
2024-01-16 19:59:25 -0500

    Started changing clocks config

--- src/pal_9k4/clocks.c
+++ src/pal_9k4/clocks.c
@@ -1,6 +1,6 @@
 #include "clocks.h"
 
-#include "stm32g4xx_hal.h"
+#include "stm32h7xx_hal.h"
 
 void SystemClock_Config() {
     // CORE FREQUENCY: 168Mhz
@@ -10,7 +10,10 @@ void SystemClock_Config() {
 
     /** Configure the main internal regulator output voltage
      */
-    HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1_BOOST);
+    HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
+    HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE0);
+    while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {
+    }
 
     /** Initializes the RCC Oscillators according to the specified parameters
      * in the RCC_OscInitTypeDef structure.
@@ -24,52 +27,66 @@ void SystemClock_Config() {
     RCC_OscInitStruct.HSI48State = RCC_HSI48_ON;
     RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
     RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV3;
-    RCC_OscInitStruct.PLL.PLLN = 21;
-    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-    RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-    RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+    RCC_OscInitStruct.PLL.PLLM = 16;
+    RCC_OscInitStruct.PLL.PLLN = 275;
+    RCC_OscInitStruct.PLL.PLLP = 1;
+    RCC_OscInitStruct.PLL.PLLQ = 1;
+    RCC_OscInitStruct.PLL.PLLR = 2;
+    RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_1;
+    RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
+    RCC_OscInitStruct.PLL.PLLFRACN = 0;
     HAL_RCC_OscConfig(&RCC_OscInitStruct);
 
     /** Initializes the CPU, AHB and APB buses clocks
      */
     RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
-                                  RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+                                  RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 |
+                                  RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV8;
-    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4);
+    RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
+    RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3);
 
-    RCC_PeriphCLKInitTypeDef periph_conf = {
-        .I2c1ClockSelection = RCC_I2C1CLKSOURCE_HSI,
-        .I2c2ClockSelection = RCC_I2C2CLKSOURCE_HSI,
-        .I2c3ClockSelection = RCC_I2C3CLKSOURCE_HSI,
-        .I2c4ClockSelection = RCC_I2C4CLKSOURCE_HSI,
-    };
+    /** Initializes the peripherals clock
+     */
+    RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+    PeriphClkInitStruct.PeriphClockSelection =
+        RCC_PERIPHCLK_OSPI | RCC_PERIPHCLK_SDMMC | RCC_PERIPHCLK_SPI123 |
+        RCC_PERIPHCLK_I2C1235 | RCC_PERIPHCLK_I2C4;
+    PeriphClkInitStruct.PLL2.PLL2M = 32;
+    PeriphClkInitStruct.PLL2.PLL2N = 160;
+    PeriphClkInitStruct.PLL2.PLL2P = 8;
+    PeriphClkInitStruct.PLL2.PLL2Q = 2;
+    PeriphClkInitStruct.PLL2.PLL2R = 8;
+    PeriphClkInitStruct.PLL2.PLL2RGE = RCC_PLL2VCIRANGE_0;
+    PeriphClkInitStruct.PLL2.PLL2VCOSEL = RCC_PLL2VCOMEDIUM;
+    PeriphClkInitStruct.PLL2.PLL2FRACN = 0;
+    PeriphClkInitStruct.OspiClockSelection = RCC_OSPICLKSOURCE_PLL2;
+    PeriphClkInitStruct.SdmmcClockSelection = RCC_SDMMCCLKSOURCE_PLL2;
+    PeriphClkInitStruct.Spi123ClockSelection = RCC_SPI123CLKSOURCE_PLL2;
+    PeriphClkInitStruct.I2c1235ClockSelection = RCC_I2C1235CLKSOURCE_HSI;
+    PeriphClkInitStruct.I2c4ClockSelection = RCC_I2C4CLKSOURCE_HSI;
 
-    periph_conf.PeriphClockSelection = RCC_PERIPHCLK_I2C1;
-    HAL_RCCEx_PeriphCLKConfig(&periph_conf);
-    periph_conf.PeriphClockSelection = RCC_PERIPHCLK_I2C2;
-    HAL_RCCEx_PeriphCLKConfig(&periph_conf);
-    periph_conf.PeriphClockSelection = RCC_PERIPHCLK_I2C3;
-    HAL_RCCEx_PeriphCLKConfig(&periph_conf);
-    periph_conf.PeriphClockSelection = RCC_PERIPHCLK_I2C4;
-    HAL_RCCEx_PeriphCLKConfig(&periph_conf);
-
-    periph_conf.PeriphClockSelection = RCC_PERIPHCLK_QSPI;
-    periph_conf.QspiClockSelection = RCC_QSPICLKSOURCE_SYSCLK;
-    HAL_RCCEx_PeriphCLKConfig(&periph_conf);
+    HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
 
     // Initialize I2C clocks
+    __HAL_RCC_I2C1_CLK_ENABLE();
     __HAL_RCC_I2C2_CLK_ENABLE();
     __HAL_RCC_I2C3_CLK_ENABLE();
     __HAL_RCC_I2C4_CLK_ENABLE();
+    __HAL_RCC_I2C5_CLK_ENABLE();
 
     // Initialize SPI clocks
     __HAL_RCC_SPI1_CLK_ENABLE();
     __HAL_RCC_SPI2_CLK_ENABLE();
+    __HAL_RCC_SPI3_CLK_ENABLE();
     __HAL_RCC_SPI4_CLK_ENABLE();
+    __HAL_RCC_SPI5_CLK_ENABLE();
+    __HAL_RCC_SPI6_CLK_ENABLE();
 
     // Initialize GPIO clocks
     __HAL_RCC_GPIOA_CLK_ENABLE();
@@ -78,9 +95,9 @@ void SystemClock_Config() {
     __HAL_RCC_GPIOD_CLK_ENABLE();
     __HAL_RCC_GPIOE_CLK_ENABLE();
     __HAL_RCC_GPIOF_CLK_ENABLE();
+    __HAL_RCC_GPIOG_CLK_ENABLE();
+    __HAL_RCC_GPIOH_CLK_ENABLE();
 
     // Initialize QSPI
-    __HAL_RCC_QSPI_CLK_ENABLE();
-    __HAL_RCC_QSPI_FORCE_RESET();
-    __HAL_RCC_QSPI_RELEASE_RESET();
+    __HAL_RCC_OCTOSPIM_CLK_ENABLE();
 }
