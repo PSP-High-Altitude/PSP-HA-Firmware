d813b6e279a786baad19d4341b32dd7e3d08438f
hkadlec12 <102488862+hkadlec12@users.noreply.github.com>
2024-01-21 12:19:48 -0500

    Update state_est.h

--- lib/control/state_est.h
+++ lib/control/state_est.h
@@ -2,19 +2,22 @@
 #define STATE_EST_H
 
 #include "data.h"
+#include "vector.h"
 
-typedef struct Vector {  // this struct should probably go somehwere else
-    float x;
-    float y;
-    float z;
-} Vector;
+// typedef struct Vector {  // this struct should probably go somehwere else
+//     float x;
+//     float y;
+//     float z;
+// } Vector;
 
 typedef struct {
+    // x is up!
     Vector posNED;
     Vector velNED;
     Vector accNED;
     Vector velBody;
     Vector accBody;
+    Vector orientation;
 } StateEst;
 
 StateEst update_state(SensorData* data, FlightPhase* fp,
