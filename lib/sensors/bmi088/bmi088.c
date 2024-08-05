4360e1826be26f7dbcc9c93fe52f55f8f57dd8bc
CodingGenius64 <lukecwshannon@gmail.com>
2024-08-04 20:55:19 -0400

    New Code (LUCAS)
    Stuff I did for BMI088

---
+++ lib/sensors/bmi088/bmi088.c
@@ -0,0 +1,137 @@
+#include "bmi088.h"
+
+#include <math.h>
+#include <string.h>
+
+#include "timer.h"
+
+// Initialize
+uint8_t current_acc_range;
+uint8_t current_gyro_range;
+double acc_conversion[] = {1.0 / 10920.0, 1.0 / 5460.0, 1.0 / 2730.0,
+                           1 / 1365.0};
+double gyro_conversion[] = {1.0 / 16.384, 1.0 / 32.768, 1.0 / 65.536,
+                            1.0 / 131.072, 1.0 / 262.144};
+
+Status bmi088_init(I2cDevice* acc_device, I2cDevice* gyro_device,
+                   Bmi088GyroRate gyro_rate, Bmi088AccRate acc_rate,
+                   Bmi088GyroRange gyro_range, Bmi088AccRange acc_range) {
+    current_acc_range = acc_range;
+    current_gyro_range = gyro_range;
+    uint8_t buf[2];
+
+    // Read BMI_ACC & BMI_Gyro register to confirm we're connected
+    buf[0] = BMI088_ACC_CHIP_ID;
+    if (i2c_write(acc_device, buf, 1) != STATUS_OK) {
+        return STATUS_ERROR;
+    }
+    if (i2c_read(acc_device, buf, 1) != STATUS_OK) {
+        return STATUS_ERROR;
+    }
+    if (buf[0] != 0x1e) {
+        return STATUS_ERROR;
+    }
+
+    buf[0] = BMI088_GYRO_CHIP_ID;
+    if (i2c_write(gyro_device, buf, 1) != STATUS_OK) {
+        return STATUS_ERROR;
+    }
+    if (i2c_read(gyro_device, buf, 1) != STATUS_OK) {
+        return STATUS_ERROR;
+    }
+    if (buf[0] != 0x1f) {
+        return STATUS_ERROR;
+    }
+
+    // Perform configurations for ACC and activate
+    buf[0] = BMI088_ACC_CONF;
+    buf[1] = 0xA0 | acc_rate;
+    if (i2c_write(acc_device, buf, 2) != STATUS_OK) {
+        return STATUS_ERROR;
+    }
+
+    return STATUS_OK;
+
+    buf[0] = BMI088_ACC_RANGE;
+    buf[1] = acc_range;
+    if (i2c_write(acc_device, buf, 2) != STATUS_OK) {
+        return STATUS_ERROR;
+    }
+
+    return STATUS_OK;
+
+    buf[0] = BMI088_ACC_PWR_CONF;
+    buf[1] = 0x00;
+    if (i2c_write(gyro_device, buf, 2) != STATUS_OK) {
+        return STATUS_ERROR;
+    }
+
+    return STATUS_OK;
+
+    buf[0] = BMI088_ACC_PWR_CTRL;
+    buf[1] = 0x04;
+    if (i2c_write(gyro_device, buf, 2) != STATUS_OK) {
+        return STATUS_ERROR;
+    }
+
+    return STATUS_OK;
+
+    // Perform configurations for Gyro
+    buf[0] = BMI088_GYRO_RANGE;
+    buf[1] = gyro_range;
+    if (i2c_write(gyro_device, buf, 2) != STATUS_OK) {
+        return STATUS_ERROR;
+    }
+
+    return STATUS_OK;
+
+    buf[0] = BMI088_GYRO_BANDWIDTH;
+    buf[1] = 0x80 | gyro_rate;
+    if (i2c_write(gyro_device, buf, 2) != STATUS_OK) {
+        return STATUS_ERROR;
+    }
+
+    return STATUS_OK;
+}
+
+// Read the data from the Acc device
+Accel bmi088_acc_read(I2cDevice* device) {
+    uint8_t buf[6];
+    Accel accel = {NAN, NAN, NAN};
+
+    buf[0] = BMI088_ACC_OUT;
+
+    if (i2c_write(device, buf, 1) != STATUS_OK) {
+        return accel;
+    }
+    if (i2c_read(device, buf, 6) != STATUS_OK) {
+        return accel;
+    }
+    double cf = acc_conversion[current_acc_range];
+    accel.accelX = (int16_t)(((uint16_t)buf[1] << 8) | (uint16_t)buf[0]) * cf;
+    accel.accelY = (int16_t)(((uint16_t)buf[3] << 8) | (uint16_t)buf[2]) * cf;
+    accel.accelZ = (int16_t)(((uint16_t)buf[5] << 8) | (uint16_t)buf[4]) * cf;
+
+    return accel;
+}
+
+// Read data from Gyro device
+Gyro bmi088_gyro_read(I2cDevice* device) {
+    uint8_t buf[6];
+    Gyro gyro = {NAN, NAN, NAN};
+
+    buf[0] = BMI088_GYRO_OUT;
+
+    if (i2c_write(device, buf, 1) != STATUS_OK) {
+        return gyro;
+    }
+    if (i2c_read(device, buf, 6) != STATUS_OK) {
+        return gyro;
+    }
+    double cf = gyro_conversion[current_gyro_range];
+    gyro.gyroX = (int16_t)(((uint16_t)buf[1] << 8) | (uint16_t)buf[0]) * cf;
+    gyro.gyroY = (int16_t)(((uint16_t)buf[3] << 8) | (uint16_t)buf[2]) * cf;
+    gyro.gyroZ = (int16_t)(((uint16_t)buf[5] << 8) | (uint16_t)buf[4]) * cf;
+
+    return gyro;
+}
