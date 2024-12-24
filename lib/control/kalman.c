009abea557cace811a72d11bd73531907493c1f5
hkadlec12 <102488862+hkadlec12@users.noreply.github.com>
2024-12-24 00:17:48 -0600

    Matrix Setup
    attempted some memory allocation

--- lib/control/kalman.c
+++ lib/control/kalman.c
@@ -3,9 +3,91 @@
 #include "arm_math.h"
 #include "flight_control.h"
 #include "state_estimation.h"
+#include "stdlib.h"
 
-void init_state(State* state_ptr, int num_states, const float32_t* x0,
-                const float32_t* P0) {
-    arm_mat_init_f32(&(state_ptr->x), num_states, num_states, x0);
-    arm_mat_init_f32(&(state_ptr->P), num_states, num_states, P0);
+// MATRICES (STATIC)
+static KfState state;  // might not need this
+static mat x;          // state
+static mat P;          // state covar mat
+static mat F;          // state update mat
+static mat Q;          // process noise mat
+static mat H;          // state to meas mat
+static mat R;          // meas noise mat
+static mat y;          // residual
+static mat z;          // measurements excluding rotation
+static mat w;          // angular velocity measurements
+
+static mfloat Q_vars = {1., 1., 1., 1., 1., 1., 1.};
+
+// FUNCTION DEFINITIONS
+void init_mats() {
+    // there's probably a less mallocy way to do this but for now this should
+    // work (The arm_matrix_instance_f32 just points to an array of the data, so
+    // it needs to be allocated somewhere)
+    kf_matalloc(&x, NUM_TOT_STATES, 1);
+    kf_matalloc(&P, NUM_TOT_STATES, NUM_TOT_STATES);
+    kf_matalloc(&F, NUM_TOT_STATES, NUM_TOT_STATES);
+    kf_matalloc(&Q, NUM_TOT_STATES, NUM_TOT_STATES);
+    kf_matalloc(&H, NUM_KIN_MEAS, NUM_TOT_STATES);
+    kf_matalloc(&R, NUM_KIN_MEAS, NUM_KIN_MEAS);
+    kf_matalloc(&y, NUM_KIN_MEAS, 1);
+    kf_matalloc(&z, NUM_KIN_MEAS, 1);
+    kf_matalloc(&w, 4, 1);  // 4 because quaternions
+    state.x = &x;
+    state.P = &P;
 }
+void kf_matalloc(mat* mat_ptr, uint16_t rows, uint16_t cols) {
+    // allocates memory for the matrix
+    // yes it's malloc but idk a better way, if someone else does feel free to
+    // change it
+    arm_mat_init_f32(mat_ptr, rows, cols, malloc(sizeof(mfloat) * rows * cols));
+}
+
+void kf_edit_mat(mat* mat_ptr, uint16_t i, uint16_t j, mfloat value) {
+    mat_ptr->pData[i * mat_ptr->numCols + j] = value;
+}
+
+void kf_init_state(int num_states, const mfloat* x0, const mfloat* P0) {
+    arm_mat_init_f32(&(state.x), num_states, num_states, x0);
+    arm_mat_init_f32(&(state.P), num_states, num_states, P0);
+}
+
+void kf_diag(mat* mptr, const mfloat* diag_vals, bool zeros) {
+    // puts diag vals on the diagonal of the matrix. If zeros is true it fills
+    // the rest with zeros. Otherwise it leaves the other elements alone.
+    for (int i = 0; i < mptr->numRows; i++) {
+        for (int j = 0; j < mptr->numCols; j++) {
+            if (i == j) {
+                kf_edit_mat(mptr, i, j, diag_vals[i]);
+            } else if (zeros) {
+                kf_edit_mat(mptr, i, j, 0);
+            }
+        }
+    }
+}
+bool kf_do_kf(StateEst* state_ptr, FlightPhase phase,
+              const SensorFrame* sensor_frame);
+
+void kf_predict(KfState* state_ptr, int dt) {
+    // kf_Q_matrix(dt)
+    kf_F_matrix(dt);    // update F matrix with dt
+    kf_fx(&x, dt, &w);  // calculate (integrate) new state
+
+    // REF
+    // self.Q = np.diag(self.Q_var*dt)
+    // F = self.F(dt)
+
+    // self.x = self.f(self.x, dt, w=w) # use f(x) for EKF
+    // self.P = F @ self.P @ F.T + self.Q
+}
+
+void kf_update(KfState* state_ptr, const SensorFrame* sensor_frame,
+               const mfloat* R_diag);
+
+void kf_preprocess(const SensorFrame* sensor_frame, KfState* state_ptr,
+                   FlightPhase phase);
+
+void kf_Q_matrix(int dt);
+void kf_F_matrix(int dt) {}
+void kf_R_matrix(const mfloat* meas_vars);
+void kf_H_matrix();
--- lib/control/kalman.h
+++ lib/control/kalman.h
@@ -5,31 +5,60 @@
 #include "flight_control.h"
 #include "state_estimation.h"
 
-// TODO: Make all the matrices static variables
+#define NUM_KIN_STATES (3)  // non-rotational states (h,v,a)
+#define NUM_ROT_STATES (4)  // rotation states (quaternion)
+#define NUM_KIN_MEAS (3)
+#define NUM_ROT_MEAS (3)
+#define NUM_TOT_STATES (7)      // total states,
+#define TIME_CONVERSION (1E6f)  //
+// does time come in as seconds or ns
 
+//////////// NOTES ON MATRICES ////////////
+// The mat struct (arm_matrix_instance_f32) contains the number of rows, number
+// of cols, and pointer to data. It does not allocate the space for data (see
+// kf_matalloc for that)
+// i = row
+// j = col
+// mat[i,j] = mat->pData[i*mat->numCols + j]
+///////////////////////////////////////////
+
+// STRUCT DEFINITIONS
 typedef struct {
-    arm_matrix_instance_f32 x;
-    arm_matrix_instance_f32 P;
+    mat* x;
+    mat* P;
     /* data */
-} State;
+} KfState;
 
-void kf_init_state(int num_states, const float32_t* x0, const float32_t* P0);
+// change these if we want diferent sized floats
+typedef arm_matrix_instance_f32 mat;
+typedef float32_t mfloat;
 
-void kf_init_diag(arm_matrix_instance_f32* mat_ptr, const float32_t* diag_vals);
+// typedef struct {
+//     arm_matrix_instance_f32 matrix
+//     float32_t[] mat_ptr
+// }
+
+void kf_init_mats();
+void kf_matalloc(mat* mat_ptr, uint16_t rows, uint16_t cols);
+void kf_init_state(int num_states, const mfloat* x0, const mfloat* P0);
+void kf_edit_mat(mat* mat_ptr, uint16_t i, uint16_t j, mfloat value);
+
+void kf_diag(mat* mat_ptr, const mfloat* diag_vals, bool zeros);
 bool kf_do_kf(StateEst* state_ptr, FlightPhase phase,
               const SensorFrame* sensor_frame);
 
-void kf_predict(State* state_ptr, int dt);
+void kf_predict(KfState* state_ptr, int dt);
 
-void kf_update(State* state_ptr, const SensorFrame* sensor_frame,
-               const float32_t* R_diag);
+void kf_update(KfState* state_ptr, const SensorFrame* sensor_frame,
+               const mfloat* R_diag);
 
-void kf_preprocess(const SensorFrame* sensor_frame, State* state_ptr,
+void kf_preprocess(const SensorFrame* sensor_frame, KfState* state_ptr,
                    FlightPhase phase);
 
-arm_matrix_instance_f32 kf_Q_matrix(const float32_t* Q_diag, int dt);
-arm_matrix_instance_f32 kf_F_matrix(int dt);
-arm_matrix_instance_f32 kf_R_matrix(const float32_t* meas_vars);
-arm_matrix_instance_f32 kf_H_matrix();
+void kf_Q_matrix(int dt);
+void kf_F_matrix(int dt);
+void kf_R_matrix(const mfloat* meas_vars);
+void kf_H_matrix();
+void kf_fx(mat* x, int dt, mat* w);
 
 #endif  // KALMAN_H
