3354f7bd85f01d8e05089ead06749ec9287d6cfe
hkadlec12 <102488862+hkadlec12@users.noreply.github.com>
2025-03-12 19:09:33 -0400

    plots

---
+++ scripts/kf_plots.py
@@ -0,0 +1,35 @@
+# Import('env')
+import sys
+sys.path.append(r'C:\Users\hkadl\OneDrive - purdue.edu\Documents\PSP\Flight-Analysis\PAL Post Flight')
+from darkstar_plots import *
+
+from pal_plots import load_sensor_data, plot_pressure, plot_pressure_alt
+from darkstar_plots import *
+
+# c_out_file = r'C:\Users\hkadl\OneDrive - purdue.edu\Documents\PSP\PSP-HA-Firmware\lib\control\kf_out.csv'
+# data_in_file2 = r'C:\Users\hkadl\OneDrive - purdue.edu\Documents\PSP\Python\Data Files\pal_test\pal_fsl_test_dat.csv'
+# data_in_file = r'C:\Users\hkadl\OneDrive - purdue.edu\Documents\PSP\Python\Data Files\pal_test\dat_trimmed.csv'
+data_in_file = r'C:\Users\hkadl\OneDrive - purdue.edu\Documents\PSP\PSP-HA-Firmware\sim_out\sensor.csv'
+kf_out_file = r'C:\Users\hkadl\OneDrive - purdue.edu\Documents\PSP\PSP-HA-Firmware\sim_out\state.csv'
+# NEW_NAMES = ["timestamp",	"flight_phase",	"pos_geo_x",	"pos_geo_y",	"pos_geo_z",	"vel_geo_x",	"vel_geo_y",	"vel_geo_z",	"acc_geo_x",	"acc_geo_y",    "acc_geo_z", 	"pos_geo_x",	"pos_geo_y",	"pos_geo_z",	"vel_geo_x",	"vel_geo_y",	"vel_geo_z",	"acc_geo_x",	"acc_geo_y",	"acc_geo_z",    "angvel_body_x",	"angvel_body_y",	"angvel_body_z",	"orient_geo_w",	"orient_geo_x",	"orient_geo_y",	"orient_geo_z"]
+
+
+data = load_data(data_in_file)
+
+
+
+kf_state = load_data(kf_out_file)
+vert_state_names = ['pos_vert', 'vel_vert', 'acc_vert']
+kf_names = ['pos_ekf', 'vel_ekf', 'acc_ekf']
+# plot_pressure(data)
+# plot_kf_se(kf_state, name="kf state")
+name = "SWIL"
+plot_compare_vertical_se(kf_state, kf_state, kf_names, vert_state_names, name=name, legnames=["kf state", "state"])
+plot_altitude_pressure(kf_state, data, kf_names, vert_state_names, name)
+plot_kf_se(kf_state, name=name)
+
+
+plt.show()
+plt.pause(.1) # make plot not disappear in debug mode
+
+# TODO: Trim to match range
